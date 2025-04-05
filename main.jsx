91af725a0cce75321214c5da85802a35eb3f3a24
kerlosT <cartru1989@gmail.com>
2025-04-05 10:47:34 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import IngeBotApp from './IngeBotApp'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <IngeBotApp />
-  </React.StrictMode>
-)
