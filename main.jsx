7a6ea759ff3dd4a0ee3df41234fd47ad36604bdd
kerlosT <cartru1989@gmail.com>
2025-04-05 10:08:55 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IngeBotApp from './IngeBotApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IngeBotApp />
+  </React.StrictMode>
+)
